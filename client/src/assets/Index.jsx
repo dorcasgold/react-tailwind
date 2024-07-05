ed95a9006db19ca2c9d53968f72626dccf695ab2
odetayo dorcas <odetayodorcas123@gmail.com>
2024-07-05 17:54:46 +0100

    Header

---
+++ client/src/assets/Index.jsx
@@ -0,0 +1,9 @@
+import logoDark from './logoDark.png'
+import logoLight from './logoLight.png'
+import googleLogo from './googleLogo.png'
+import githubLogo from './githubLogo.png'
+import paymentLogo from './paymentLogo.png'
+import cartpic from './cart.png'
+import userLogo from './profile-pic.JPG'
+
+export { logoDark, logoLight, googleLogo, githubLogo, paymentLogo, cartpic, userLogo }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { logoDark } from '../assets/Index'
+import { cartpic } from '../assets/Index'
+import { userLogo } from '../assets/Index'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className="w-full h-20 bg-white border-b-[1px] border-b-gray-800">
+      <div className="max-w-screen-xl h-full mx-5 flex items-center justify-between">
+        <div>
+          <img className='w-28' src={logoDark} alt="logoDark" />
+        </div>
+
+        <div className='flex items-center gap-8'>
+          <ul className='flex items-center gap-8'>
+            <li className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-3000'>Home</li>
+            <li className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-3000'>Pages</li>
+            <li className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-3000'>Shop</li>
+            <li className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-3000'>Blog</li>
+          </ul>
+          <div className=' relative'>
+            <img src={cartpic} alt="cartpic" className='w-6' />
+            <span className=' absolute w-6 top-2 left-0 text-sm flex items-center justify-center font-semibold'>0</span>
+          </div>
+          <img src={userLogo} alt="userLogo" className='w-8 h-8 rounded-full' />
+        </div>
+      </div>
+    </div>
   )
 }
 
